76a1041b61eba43796b5a8a600e469df9da8ee4e
Thomas Draper <tom@betterservers.com>
2017-01-27 12:23:08 -0700

    Add virtualenv support.

--- bin/Mint18.sh
+++ bin/Mint18.sh
@@ -2,16 +2,18 @@
 
 # Install proprietary nvidia driver in Settings 
 
-TERMINAL="direnv mmv moreutils tmux vim zsh"
+TERMINAL="direnv mmv moreutils tmux vim virtualenv virtualenvwrapper zsh"
 TYPESCRIPT="rst2pdf sphinx-common texlive-full"
 DOCS="kdiff3-doc sqlite3-doc zsh-doc"
+GRAPHIC="inkscape"
 PYTHON="ipython python-bottle python-coverage python-daemon python-flask python-future python-matplotlib python-numpy python-pandas python-requests python-pip python-scipy python-setuptools python-tk"
 PYTHON3="ipython3 python3-bottle python3-coverage python3-daemon python3-flask python3-future python3-matplotlib python3-numpy python3-requests python3-pip python3-setuptools python3-tk"
 PROG="git gitk kdiff3 sqlite3 tox"
 SERVER="iperf openssh-server"
+DATABASE="sqlitebrowser"
 USER="chromium-browser eog gedit pidgin"
 
-sudo apt install ${TERMINAL} ${TYPESCRIPT} ${DOCS} ${PYTHON} ${PYTHON3} ${PROG} ${SERVER} ${USER}
+sudo apt install ${TERMINAL} ${TYPESCRIPT} ${DOCS} ${PYTHON} ${PYTHON3} ${PROG} ${SERVER} ${USER} ${GRAPHIC} ${DATABASE}
 
 # Manual setup
 # Sign into gmail
