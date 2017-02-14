21e831c65af333b96ffeb50048c005ce18831cf8
Thomas Draper <tom@betterservers.com>
2017-02-14 10:03:12 -0700

    Add JSON schema for python.

--- bin/Mint18.sh
+++ bin/Mint18.sh
@@ -3,11 +3,11 @@
 # Install proprietary nvidia driver in Settings 
 
 TERMINAL="direnv mmv moreutils tmux vim virtualenv virtualenvwrapper zsh"
-TYPESCRIPT="rst2pdf sphinx-common texlive-full"
+TYPESCRIPT="enscript rst2pdf sphinx-common texlive-full"
 DOCS="kdiff3-doc sqlite3-doc zsh-doc"
 GRAPHIC="inkscape"
-PYTHON="ipython python-bottle python-coverage python-daemon python-eventlet python-flask python-future python-matplotlib python-numpy python-pandas python-requests python-pip python-scipy python-setuptools python-tk"
-PYTHON3="ipython3 python3-bottle python3-coverage python3-daemon python3-eventlet python3-flask python3-future python3-matplotlib python3-numpy python3-requests python3-pip python3-setuptools python3-tk"
+PYTHON="ipython python-bottle python-coverage python-daemon python-eventlet python-flask python-future python-jsonschema python-matplotlib python-nine python-numpy python-pandas python-requests python-pip python-scipy python-setuptools python-tk"
+PYTHON3="ipython3 python3-bottle python3-coverage python3-daemon python3-eventlet python3-flask python3-future python3-jsonschema python3-matplotlib python3-nine python3-numpy python3-pandas python3-requests python3-pip python3-setuptools python3-tk"
 PROG="git gitk kdiff3 sqlite3 tox"
 SERVER="iperf openssh-server"
 DATABASE="sqlitebrowser"
