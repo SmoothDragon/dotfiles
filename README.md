254c1a537c9a324830a12f44b1c140c94abd28fc
Thomas Draper <tom@betterservers.com>
2017-07-24 13:18:25 -0600

    Add graphic programs

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ Get different python module working for a directory:
   pyenv install 3.6.1 # Current available version
   Add line to .envrc:
     use python 3.6.1
-  pip3 install virtualenv # Seems to be necessary before next step
-  pip3 install ipython3   # Get ipython to be same version
+  pip3 install ipython   # Get ipython to be same version
+
+Not needed anymore?
+  #pip3 install virtualenv # Seems to be necessary before next step
--- bin/Mint18.sh
+++ bin/Mint18.sh
@@ -3,11 +3,12 @@
 # Install proprietary nvidia driver in Settings 
 
 TERMINAL="direnv mmv moreutils tmux vim virtualenv virtualenvwrapper zsh"
-TYPESCRIPT="enscript pdftk rst2pdf sphinx-common texlive-full"
+TYPESCRIPT="enscript pdftk rst2pdf sphinx-common texlive-full wkhtmltopdf"
 DOCS="kdiff3-doc sqlite3-doc zsh-doc"
-GRAPHIC="inkscape"
-PYTHON="ipython python-bottle python-coverage python-daemon python-eventlet python-flask python-future python-jsonschema python-matplotlib python-nine python-numpy python-pandas python-requests python-pip python-scipy python-setuptools python-tk"
-PYTHON3="ipython3 python3-bottle python3-coverage python3-daemon python3-eventlet python3-flask python3-future python3-gevent python3-jsonschema python3-matplotlib python3-nine python3-numpy python3-pandas python3-pika python3-requests python3-pip python3-setuptools python3-tk"
+GRAPHIC="graphviz inkscape"
+EVERNOTE="libcurl3 tidy"
+PYTHON="ipython python-bottle python-coverage python-daemon python-eventlet python-flask python-future python-jsonschema python-matplotlib python-networkx python-nine python-numpy python-pandas python-requests python-pip python-scipy python-setuptools python-tk"
+PYTHON3="ipython3 python3-bottle python3-coverage python3-daemon python3-eventlet python3-flask python3-future python3-gevent python3-jsonschema python3-matplotlib python3-networkx python3-nine python3-numpy python3-pandas python3-pika python3-requests python3-pip python3-setuptools python3-tk"
 PROG="git gitk kdiff3 sqlite3 tox"
 SERVER="iperf openssh-server openvpn rabbitmq-server"
 DATABASE="sqlitebrowser libsqlite3-dev"
