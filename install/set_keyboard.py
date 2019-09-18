d98301d68dff46535e60aeab0b5c9f3de3fd7543
Thomas Draper <tom@betterservers.com>
2019-09-18 16:36:57 -0700

    Add script for setting hotkeys in Gnome.

---
+++ install/set_keyboard.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+''' Script to set keyboard shortcuts in a Gnome session.
+
+If script fails to connect to dconf server, try running this first:
+    export $(dbus-launch)
+'''
+
+import subprocess
+import sys
+
+shortcuts = [
+    {
+        'name': 'Lock screen',
+        'command': 'xdg-screensaver lock',
+        'binding': '<Super><Alt>l',
+    },
+    {
+        'name': 'logout',
+        # 'command': 'killall -u `whoami`',
+        'command': 'gnome-session-quit --logout --no-prompt',
+        'binding': '<Super><Alt>q',
+    },
+    {
+        'name': 'Terminal',
+        'command': 'gnome-terminal',
+        'binding': '<Super><Alt>t',
+    },
+    {
+        'name': 'Big Terminal',
+        'command': 'gnome-terminal --maximize',
+        'binding': '<Ctrl><Alt>t',
+    },
+    {
+        'name': 'Google Chrome',
+        'command': '/n/common/google-chrome/curr/google-chrome',
+        'binding': '<Super><Alt>z',
+    },
+]
+
+def execute_command(cmd):
+    subprocess.call(["/bin/bash", "-c", cmd])
+
+def bindkey(index, description):
+    command = 'gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom{index}/ {key} "{value}"'
+    for key,value in description.items():
+        cmd = command.format(**locals())
+        # print(cmd)
+        execute_command(cmd)
+
+def register_keys(num_keys):
+    item = '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom{index}/'
+    bindings = [item.format(**locals()) for index in range(num_keys)]
+    command = 'gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "{bindings}"'
+    cmd = command.format(**locals())
+    # print(cmd)
+    execute_command(cmd)
+
+if __name__ == '__main__':
+    for ii, description in enumerate(shortcuts):
+        bindkey(ii, description)
+    register_keys(len(shortcuts))
+
