f055a1e9f953569f00fd0eae13a0fb02004e16a7
Thomas Draper <tgdrape@gmail.com>
2022-03-24 21:48:17 -0700

    Add wallpapers and keyboard shortcuts.

--- sbin/basic.sh
+++ sbin/basic.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo apt install -y make zsh
+sudo apt install -y curl hddtemp lm-sensors net-tools openssh-server make tmux zsh
+
+# If you have static and delayed sound
+# killall speech-dispatcher
+# sudo apt remove speech-dispatcher
